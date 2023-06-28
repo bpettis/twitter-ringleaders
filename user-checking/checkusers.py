39d39080f05bf1f6959203e6ca529b1f37bd51c6
Ben Pettis <bpettis@wisc.edu>
2023-06-28 16:01:19 -0500

    setup structure to search pages

--- user-checking/checkusers.py
+++ user-checking/checkusers.py
@@ -7,15 +7,38 @@ filename = 'put/file/here.csv'
 
 def main():
     print(f'Now checking usernames from {filename}')
-    test_selenium()
 
-def test_selenium():
+    headlesschrome = setup_selenium()
+
+
+    url = 'https://example.com'
+    page_text = load_page(headlesschrome, url)
+    print(page_text)
+
+    search_page(page_text)
+
+    # Quit/Close the headless chrome browser
+    if headlesschrome: 
+        headlesschrome.quit()
+
+def setup_selenium():
     options = ChromeOptions()
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
-    driver.get("https://duckduckgo.com/")
-    print(driver.current_url)
-    driver.quit()
+    driver.get("https://example.com")
+    print(f'Loaded: {driver.current_url}')
+    return driver
+
+# load a specified URL and return the page source as text
+def load_page(driver, url):
+    driver.get(url)
+    return driver.page_source
+
+def search_page(page):
+    term = 'Example'
+    # print True if text is present else False
+    print(term in page)
+
 
 
 
